1c5191c85971b54ecb0de06351c90089533495fa
Lucas Henrique Matos <lucashenrique.mtos@gmail.com>
2024-06-16 00:20:19 -0300

    Subindo arquivos

---
+++ cliente/src/test/java/br/com/lhmatos/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lhmatos.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
